8f1e49900b2527b85af188a2b4d2f758d7ae57e8
haozheng <hao.zheng@presslogic.com>
2021-06-25 20:23:01 +0800

    脚本调整

--- scripts/add-key.sh
+++ scripts/add-key.sh
@@ -3,8 +3,10 @@
 security create-keychain -p travis ios-build.keychain
 security default-keychain -d user -s ios-build.keychain
 security unlock-keychain -p travis ios-build.keychain
-security set-keychain-settings -t 3600 -l ~/Library/Keychains/ios-build.keychain
+security set-keychain-settings -t 3600 -u ios-build.keychain
 
+wget https://developer.apple.com/certificationauthority/AppleWWDRCA.cer
+security import AppleWWDRCA.cer  -k  ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import profile/ios_distribution_zhenghao.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import profile/dis.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -A
 
