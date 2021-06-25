e969fc20826d8e707f90afb567a805dbe406738c
haozheng <hao.zheng@presslogic.com>
2021-06-25 15:26:09 +0800

    内容调整

--- travis/ViewController.swift
+++ travis/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         
-        print('123123123')
+        print("123123123")
         
     }
 
