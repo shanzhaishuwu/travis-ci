8eb01fe9a6de97b11a9224f11c4909377c503c76
haozheng <hao.zheng@presslogic.com>
2021-07-05 16:43:50 +0800

    调整3

--- scripts/add-key.sh
+++ scripts/add-key.sh
@@ -16,7 +16,7 @@ security set-keychain-settings -t 3600 -l ~/Library/Keychains/ios-build.keychain
 # Add certificates to keychain and allow codesign to access them
 security import ./profile/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ./profile/dis.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import ./profile/dis.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
+security import ./profile/dis.p12 -k ~/Library/Keychains/ios-build.keychain -P "0728" -T /usr/bin/codesign
 
 security set-key-partition-list -S apple-tool:,apple: -s -k travis ios-build.keychain
 
