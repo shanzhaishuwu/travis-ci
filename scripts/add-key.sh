8caa9a343e9c1cb5dd3629130274141179c3d6fe
haozheng <hao.zheng@presslogic.com>
2021-07-06 18:54:44 +0800

    脚本调整5

--- scripts/add-key.sh
+++ scripts/add-key.sh
@@ -5,7 +5,7 @@ security default-keychain -d user -s ios-build.keychain
 security unlock-keychain -p travis ios-build.keychain 
 security set-keychain-settings -t 3600 -l ~/Library/Keychains/ios-build.keychain
 
-# security import ./scripts/profile/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+security import ./scripts/profile/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ./scripts/profile/dis.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ./scripts/profile/dis.p12 -k ~/Library/Keychains/ios-build.keychain -P 0728 -A
 
