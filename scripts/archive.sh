b96652900357b08ce2404d88ab3d530fad8e4a3f
haozheng <hao.zheng@presslogic.com>
2021-07-07 15:28:10 +0800

    添加文件

--- scripts/archive.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-xcrun xcodebuild -project travis.xcodeproj -scheme travis \
-  -archivePath travis.xcarchive archive
-
-xcrun xcodebuild -exportArchive -archivePath travis.xcarchive \
-  -exportPath ./build -exportOptionsPlist ExportOptions.plist
